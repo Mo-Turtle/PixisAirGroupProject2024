84cb6d351e7391582f928b1447148d1bdfc99eb2
DESKTOP-5H6JMBT\cjss0 <Jamesonstorheim@gmail.com>
2024-10-28 19:56:26 -0500

    Created a form load on accident. Whoopsies.

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace PixisAirGroupProject2024
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
